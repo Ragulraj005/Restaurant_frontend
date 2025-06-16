353759ac46f3754431c5cbace22fb6de2807c47f
Ragulraj <124597431+Ragulraj005@users.noreply.github.com>
2025-06-16 11:07:57 +0530

    Update script.js

--- Restaurant/js/script.js
+++ Restaurant/js/script.js
@@ -1,46 +1,53 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
+
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-let section = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header .navbar a');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () => {
-
+    // Close menu and search form on scroll
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-    section.forEach(sec => {
+    document.querySelector('#search-form').classList.remove('active');
+
+
+    sections.forEach(sec => {
         let top = window.scrollY;
         let height = sec.offsetHeight;
         let offset = sec.offsetTop - 150;
         let id = sec.getAttribute('id');
+
         if (top >= offset && top < offset + height) {
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
-            });
-        };
-
+            navLinks.forEach(link => link.classList.remove('active'));
+            document.querySelector(`header .navbar a[href*=${id}]`).classList.add('active');
+        }
     });
-
-
 }
+
 document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
-
 }
+
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
-
 }
 
-var swiper = new Swiper(".home-slider", {
+// NEW: Black and White Toggle
+document.querySelector('#bw-toggle').onclick = () => {
+    document.body.classList.toggle('grayscale');
+};
+
+
+var homeSwiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-        delay: 1500,
+        delay: 1500, // Reduced delay for faster demo
         disableOnInteraction: false,
     },
     pagination: {
@@ -49,37 +56,32 @@ var swiper = new Swiper(".home-slider", {
     },
     loop: true,
 });
-var swiper = new Swiper(".review-slider", {
+
+var reviewSwiper = new Swiper(".review-slider", {
     spaceBetween: 10,
     centeredSlides: true,
     autoplay: {
-        delay: 1000,
+        delay: 1000, // Reduced delay for faster demo
         disableOnInteraction: false,
     },
-
     loop: true,
     breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        640: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 3,
-        },
-        1024: {
-            slidesPerView: 4,
-        },
-
+        0: { slidesPerView: 1 },
+        640: { slidesPerView: 2 },
+        768: { slidesPerView: 3 },
+        1024: { slidesPerView: 4 },
     },
 });
 
-function loader(){
+function loader() {
     document.querySelector('.loader-container').classList.add('fade-out');
 }
-function fadeOut(){
-    setInterval(loader,1000)
+
+function fadeOut() {
+    // Using setTimeout to ensure loader is visible for at least 1 second
+    setTimeout(() => {
+        loader();
+    }, 1000); // Loader visible for 1 second before fading out
 }
 
-window.onload =fadeOut;
+window.onload = fadeOut;
